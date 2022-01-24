0cc19852d2165c9f4ec91e658b050e9c40041eb1
arcsr <chadwayment@gmail.com>
2022-01-23 21:30:07 -0700

    adding new features and removing more local files

--- proxmox_vms.tf
+++ proxmox_vms.tf
@@ -4,5 +4,14 @@ resource "proxmox_vm_qemu" "vms" {
 	name = each.value.name	
 	target_node = each.value.target_node	
 	iso = each.value.iso
+	cores = each.value.cores
+	os_type = each.value.os_type
 	
+
+	connection {
+		type = "ssh"
+		user = "${self.ssh_user}"
+		private_key = var.ssh_key
+		host = "${self.ssh_host}"
+	}
 }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,11 @@ variable "vms" {
 				name = string
 				target_node = string
 				iso = string
-			},
+				os_type = string
+				cores = number
+				ssh_user = string
+				ssh_host = string
+			}
 					
 		)
 	)
@@ -14,11 +18,23 @@ variable "vms" {
 			name = "ubuntu-test-1"
 			target_node = "proxtest1"
 			iso = "ubuntu-20.04.3-live-server-amd64.iso"
+			os_type = "ubuntu"
+			cores = 2
+			ssh_user = "terraform"
+			ssh_host = "ubuntu-test-1"
+
 		},
 		"ubuntu-test-2" = {
 			name = "ubuntu-test-2"
 			target_node = "proxtest1"
 			iso = "ubuntu-20.04.3-live-server-amd64.iso"
+			os_type = "ubuntu"
+			cores = 2
+			ssh_user = "jack"
+			ssh_host = "ubuntu-test-2"
+				
 		}
 	}
 }
+
+variable "ssh_key" {}
