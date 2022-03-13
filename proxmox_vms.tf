15163808b2aa08303a8fa30b17b9a9ef16c35f09
arcsr <chadwayment@gmail.com>
2022-03-12 21:57:28 -0700

    removal of swp

--- proxmox_vms.tf
+++ proxmox_vms.tf
@@ -1,16 +1,23 @@
 resource "proxmox_vm_qemu" "ubuntu_vms" {
-  for_each     = var.vms
-  preprovision = true
-  name         = each.value.name
-  target_node  = var.node_name
+  for_each    = var.vms
+  name        = each.value.name
+  target_node = var.node_name
 
   iso = var.ubuntu_cloud_iso
 
   cores  = each.value.cores
   memory = each.value.memory
 
-  os_type = each.value.os_type
-  boot    = "cdn"
+  os_type  = each.value.os_type
+  boot     = "cdn"
+  sshkeys  = var.ssh_keys
+  ssh_user = "root"
+
+  connection {
+    type        = "ssh"
+    user        = self.ssh_user
+    private_key = self.ssh_private_key
+  }
 
 
   disk {
@@ -21,3 +28,8 @@ resource "proxmox_vm_qemu" "ubuntu_vms" {
 
 }
 
+
+data "template_file" "user_data" {
+  count = var.vm_count
+
+}
